3c371519725888d7d011f588fdfa8016194de118
Zhe Sun <sunzhe1996@gmail.com>
2020-12-11 02:36:52 +0800

    Added default yes to r installation

--- setupNewComputer.sh
+++ setupNewComputer.sh
@@ -28,7 +28,7 @@ sudo apt install racket -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 sudo apt update
-sudo apt install r-base r-base-dev
+sudo apt install r-base r-base-dev -y
 
 ### Git Config ###
 echo -n "Enter the global user for Git: "
