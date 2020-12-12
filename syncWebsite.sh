2e3cfdb505af2524305e38370149a6f76083e496
Zhe Sun <sunzhe1996@gmail.com>
2020-12-13 04:49:17 +0800

    Changed local dir

--- syncWebsite.sh
+++ syncWebsite.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-rsync -avz ~/Documents/github/sunzhe.info/public/ root@sunzhe.info:/var/www/sunzhe.info/ --delete
+rsync -auvzP --delete ~/Documents/web/sunzhe.info/ root@sunzhe.info:/var/www/sunzhe.info/
