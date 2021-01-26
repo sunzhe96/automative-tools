55a20ce85a4510be5317b83b62997673109eae10
Zhe Sun <sunzhe1996@gmail.com>
2021-01-26 16:32:11 +0800

    Changed backup dir

--- syncLaptop.sh
+++ syncLaptop.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-backup_dir=$(lsblk | grep sda1 | awk -F" " {'print $7'} )
+backup_dir=$(lsblk | grep sda1 | awk -F" " {'print $7'})/sz96-backup
 
-rsync -auvzP ~/Documents $backup_dir --delete
+rsync -auvzP ~/Documents ~/org-roam $backup_dir --delete
