b7fe6bd12baca6b3665ec18b3cc1a0dfc7f85f4c
Zhe Sun <sunzhe1996@gmail.com>
2021-02-07 10:46:22 +0800

    Updated regular expression

--- resou.sh
+++ resou.sh
@@ -9,14 +9,15 @@ grep href=\"/weibo resou.html | awk -F ">" '{ print NR " " $2 }' | sed 's/<*\/a/
 grep href=\"/weibo resou.html | awk -F " " '{ print $2 }' | awk -v head="$head" -F "\"" '{ print head $2 }' > links
 cat topics
 
-read -p "Enter topics number [q to quit]: " answer
+read -p "Enter topic number [any letter to quit]: " answer
 
-case "$answer" in
-    [1-50]*) topic_chose=$(paste topics links | awk -v line="$answer" 'FNR == line { print $3 }');;
-    *) rm topics links resou.html && exit 0;;
-    # *) topic_chose=$(paste topics links | awk -v line="$answer" 'FNR == line { print $3 }');;
-esac
+if [[ "${answer}" =~ ^[1-9]$|^[1-4][0-9]$|^5[0-1]$ ]]
+then
+    topic_chose=$(paste topics links | awk -v line="$answer" 'FNR == line { print $3 }')
+else
+    rm topics links resou.html && exit 0
+fi
 
-firefox "$topic_chose"
+firefox "$topic_chose"    
 rm topics links resou.html
 
