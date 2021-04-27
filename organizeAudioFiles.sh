8af7fb1cba8b291023424a774b836cc44358fa79
Zhe Sun <sunzhe1996@gmail.com>
2021-04-27 21:28:38 +0800

    Modified the algorithm for creating folders

--- organizeAudioFiles.sh
+++ organizeAudioFiles.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-year=$(date +%Y)
-month=$(date +%Y-%m)
-date=$(date +%F)
+year=$(ls | grep flac | head -1 | awk -F "-" {'print $1'})
+month=$(ls | grep flac | head -1 | awk -F "-" {'print $2'})
+day=$(ls | grep flac | head -1 | awk -F "-" {'print $3'})
+
+year_month="$year-$month"
+date="$year-$month-$day"
 
 function create_date() {
-    mkdir -v "$HOME/Audio/$year/$month/$date"
+    mkdir -v "$HOME/Audio/$year/$year_month/$date"
 }
 
 function create_month_date() {
-    mkdir -v "$HOME/Audio/$year/$month" && mkdir -v "$HOME/Audio/$year/$month/$date"
+    mkdir -v "$HOME/Audio/$year/$year_month" && mkdir -v "$HOME/Audio/$year/$year_month/$date"
 }
 
 function create_full_path() {
-    mkdir -v "$HOME/Audio/$year" && mkdir -v "$HOME/Audio/$year/$month/" && mkdir -v "$HOME/Audio/$year/$month/$date"
+    mkdir -v "$HOME/Audio/$year" && mkdir -v "$HOME/Audio/$year/$year_month/" && mkdir -v "$HOME/Audio/$year/$year_month/$date"
 }
 
 
 if [[ -d "$HOME/Audio/$year" ]]
 then
-   if [[ -d "$HOME/Audio/$year/$month" ]]
+   if [[ -d "$HOME/Audio/$year/$year_month" ]]
    then
-       if [[ ! -d "$HOME/Audio/$year/$month/$date" ]]
+       if [[ ! -d "$HOME/Audio/$year/$year_month/$date" ]]
        then
 	   create_date
        fi
@@ -32,5 +35,5 @@ else
     create_full_path
 fi
 
-mv -v ~/Audio/*.flac "$HOME/Audio/$year/$month/$date/"
+mv -v ~/Audio/*.flac "$HOME/Audio/$year/$year_month/$date/"
 echo "Finished"
